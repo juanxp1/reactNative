071901aea3eff7da404ee241ca36540919e536e5
Juan Jose Bianchi <juan-josebianchi@hotmail.com>
2023-06-13 18:20:18 -0300

    probando

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>Probando si funciona en vercel</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  texto: {
+    color: 'white',
+  }
 });
